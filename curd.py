da8b081f7206ffc09a4e17cb269a1f751f01bff7
drbin <dairubin0605@163.com>
2019-01-03 17:32:22 +0800

    MQ

--- curd.py
+++ curd.py
@@ -1,4 +1,4 @@
 import pymysql
 print("pymysql 准备完成")
 
-con = pymysql.connect(host='localhost', user='root', passwd='123456', charset='utf8')
+con = pymysql.connect(host='localhost', user='root', passwd='root', charset='utf8')
