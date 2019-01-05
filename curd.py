6617a14efb40bbdc9aa7a66d3bd8f81fa06359d0
drbin <dairubin0605@163.com>
2019-01-05 17:44:07 +0800

    MQ

--- curd.py
+++ curd.py
@@ -3,4 +3,5 @@ print("pymysql 准备完成")
 
 con = pymysql.connect(host='localhost', user='root', passwd='root', charset='utf8')
 cur = con.cursor()
-cur.execute("use admin_tbl;")
+cur.execute("use actire_tbl;")
+sql_insert = 'insert into admin_tbl (admin_id,admin_name,admin_password) values(11,11,md5(1));'
