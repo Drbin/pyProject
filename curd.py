75f89b9c39f4f2c092569491d9da635a0c2c3be9
drbin <dairubin0605@163.com>
2019-01-06 17:50:39 +0800

    MQ

--- curd.py
+++ curd.py
@@ -1,6 +1,6 @@
 import pymysql
 print("pymysql 准备完成")
-con = pymysql.connect(host='localhost', user='root', passwd='root', charset='utf8' )
+con = pymysql.connect(host='localhost', user='root', passwd='root', charset='utf8')
 cur = con.cursor()
 print("获取光标")
 cur.execute("use actire_db;")
