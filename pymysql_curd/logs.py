0f53b629f04db5f6991c7d1c5cf9666e7e879ded
drbin <dairubin0605@163.com>
2019-01-09 13:48:04 +0800

    pymysql 删除

--- pymysql_curd/logs.py
+++ pymysql_curd/logs.py
@@ -5,7 +5,7 @@ my_addr = socket.gethostbyname(my_name)
 
 def logs_on(data):
     #data = data.encode()
-    f = open("logs.txt", "a+")
+    f = open("logs.txt", "a+", encoding='utf-8')
     f.write(my_addr+"\n")
     f.write(data+"\n")
     f.close()
