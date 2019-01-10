3115cabf4dccb1b4150f351caaf0a6bb96f5dc46
drbin <dairubin0605@163.com>
2019-01-10 15:45:49 +0800

    pymysql 更新操作

--- pymysql_curd/pymysql_create.py
+++ pymysql_curd/pymysql_create.py
@@ -3,7 +3,7 @@ import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cursor = con.cursor()
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
-logs.logs_on("执行了插入，SQL语句："+sql_insert)
+logs.logs_on("执行了插入，SQL语句：")
 logs.logs_on()
 cursor.execute(sql_insert)
 con.commit()
