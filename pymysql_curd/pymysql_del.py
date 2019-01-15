3aeb612b4e10898f901f30c17eeb9beeebdd2bf3
drbin <dairubin0605@163.com>
2019-01-15 15:47:30 +0800

    pymysql 更新操作

--- pymysql_curd/pymysql_del.py
+++ pymysql_curd/pymysql_del.py
@@ -3,7 +3,7 @@ import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cur = con.cursor()
 sql_del = "DELETE FROM admin_tbl WHERE admin_id = '22'"
-logs.logs_on("执行了删除操作，SQL语句为"+sql_del)
+logs.logs_on("执行了删除操作，SQL语句为:"+sql_del)
 cur.execute(sql_del)
 con.commit()
 cur.close()
