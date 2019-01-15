9247c9c17a8c2e5b0383b9bbdfc38415ea2b980d
drbin <dairubin0605@163.com>
2019-01-15 15:18:02 +0800

    pymysql 修改

--- pymysql_curd/pymsql_updata.py
+++ pymysql_curd/pymsql_updata.py
@@ -3,3 +3,4 @@ import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cursor = con.cursor()
 sql_updata = "UPDATE type_tbl SET admin_name='admin',admin_login_name='user123' WHERE admin_id= 25 ;"
+cursor.execute(sql_updata)
