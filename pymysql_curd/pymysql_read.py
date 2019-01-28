e71897cff2e09ae3a0c0a174404a1c23ccfd44c7
drbin <dairubin0605@163.com>
2019-01-28 15:28:33 +0800

    pymysql 修改

--- pymysql_curd/pymysql_read.py
+++ pymysql_curd/pymysql_read.py
@@ -1,5 +1,5 @@
 import pymysql
 import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
-sql_read = "SELECT "
+sql_read = "SELECT * FROM "
 
