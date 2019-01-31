2dbc1359bcb651427b997df54aed35f23f90b170
drbin <dairubin0605@163.com>
2019-01-31 15:30:22 +0800

    pymysql 修改

--- pymysql_curd/pymysql_read.py
+++ pymysql_curd/pymysql_read.py
@@ -3,6 +3,6 @@ import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cursor = con.cursor()
 sql_read = "SELECT * FROM admin_tbl"
-cursor.execute()
+cursor.execute(sql)
 
 
