2d7b5968c7cd1e3b329370dc8e8f5c61b72186cf
drbin <dairubin0605@163.com>
2019-02-13 15:42:10 +0800

    pymysql 查询

--- pymysql_curd/pymysql_read.py
+++ pymysql_curd/pymysql_read.py
@@ -4,7 +4,7 @@ con = pymysql.connect(host='localhost', user='root', passwd='root', database='ac
 cursor = con.cursor()
 sql_read = "SELECT * FROM admin_tbl"
 data = cursor.execute(sql_read)
-logs.logs_on("执行了查询操作，SQ语句为：")
+logs.logs_on("执行了查询操作，SQL语句为：")
 logs.logs_on(sql_read)
 con.commit()
 cursor.close()
