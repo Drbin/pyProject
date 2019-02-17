234d97dada380aeb1030ab4eac95af5c5403097b
drbin <dairubin0605@163.com>
2019-02-17 11:45:02 +0800

    日志文件

--- pymysql_curd/pmysql_del.py
+++ pymysql_curd/pmysql_del.py
@@ -2,4 +2,4 @@ import pymysql
 import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cur = con.cursor()
-sql_del = "dele"
+sql_del = "DELETE FROM admin_tbl WHERE admin_id = '22'"
