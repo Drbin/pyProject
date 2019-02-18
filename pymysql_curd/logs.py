7048c1c86a53a4a8d0ee632e7c286024010f9c7b
drbin <dairubin0605@163.com>
2019-02-18 11:25:12 +0800

    pymysql 插入数据

--- pymysql_curd/logs.py
+++ pymysql_curd/logs.py
@@ -1,6 +1,13 @@
-def file_on(data):
+import socket
+my_name = socket.getfqdn(socket.gethostname(  ))
+my_addr = socket.gethostbyname(my_name)
+
+
+def logs_on(data):
     data = data.encode()
     f = open("logs.txt", "a+")
     f.write(data+"\n")
     f.close()
-    return 1
+
+logs_on(my_addr)
+
