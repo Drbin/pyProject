4d7388e4697657807cc2e276e4b636a92d146103
drbin <dairubin0605@163.com>
2019-02-18 11:30:27 +0800

    pymysql 插入数据

--- pymysql_curd/pymysql_add.py
+++ pymysql_curd/pymysql_add.py
@@ -4,6 +4,7 @@ import logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cursor = con.cursor()
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
+logs.logs_on("执行了"+sql_insert)
 cursor.execute(sql_insert)
 con.commit()
 cursor.close()
