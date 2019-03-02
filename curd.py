02abb2b2e0b0bdce48d6378f480c6316e9d712b0
drbin <dairubin0605@163.com>
2019-03-02 10:46:17 +0800

    MQ

--- curd.py
+++ curd.py
@@ -6,7 +6,7 @@ cursor = con.cursor()
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
 cursor.execute(sql_insert)
 print(cursor.execute(sql_insert))
-print("sql语句执行完成")
+
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
 cursor.close()
