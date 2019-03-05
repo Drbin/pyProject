2481cdcfdaaed8d0706a66168872ef4fef710925
drbin <dairubin0605@163.com>
2019-03-05 10:47:06 +0800

    MQ

--- curd.py
+++ curd.py
@@ -5,7 +5,6 @@ con = pymysql.connect(host='localhost', user='root', passwd='root', database='ac
 cursor = con.cursor()
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
 cursor.execute(sql_insert)
-
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
 cursor.close()
