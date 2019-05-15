7efa253eda0f34d3a97939adee0ff90f3864fd3f
drbin <dairubin0605@163.com>
2019-05-15 15:51:31 +0800

    pymysql 更新操作

--- pymysql_curd/pymysql_updata.py
+++ pymysql_curd/pymysql_updata.py
@@ -4,7 +4,7 @@ con = pymysql.connect(host='localhost', user='root', passwd='root', database='ac
 cursor = con.cursor()
 sql_updata = "UPDATE type_tbl SET admin_name='admin',admin_login_name='user123' WHERE admin_id= 25 ;"
 cursor.execute(sql_updata)
-logs.logs_on("执行更新操作，SQL"+)
+logs.logs_on("执行更新操作，SQL")
 logs.logs_on(sql_updata)
 con.commit()
 cursor.close()
