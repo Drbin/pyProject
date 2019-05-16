313bdcac207f5df5a3a47174153c68dad6cc158e
drbin <dairubin0605@163.com>
2019-05-16 10:59:07 +0800

    MQ

---
+++ fileBug.py
@@ -0,0 +1,12 @@
+from urllib import request
+
+def d(url):
+    print("地址： %s" % url)
+    resp = request.urlopen(url)
+    data = resp.read()
+    f = open("jd.html","wb")
+    f.write(data)
+    f.close()
+    print("%d b 数据来源于 %s" %(len(data),url))
+
+d("http://www.jd.com")
