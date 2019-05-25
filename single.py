519a2b54d6b0d44c5c2e5775322370147d85b985
drbin <dairubin0605@163.com>
2019-05-25 15:24:23 +0800

    MQ

---
+++ single.py
@@ -0,0 +1,15 @@
+
+import requests
+import json
+
+
+def main():
+    resp = requests.get('http://api.tianapi.com/guonei/?key=APIKey&num=10')
+    data_model = json.loads(resp.text)
+
+
+    print(data_model)
+
+
+if __name__ == '__main__':
+    main()
