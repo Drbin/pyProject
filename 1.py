4a4fe787e1e800d52318f134198110a129b4940b
drbin <dairubin0605@163.com>
2019-05-30 15:29:53 +0800

    MQ

--- 1.py
+++ 1.py
@@ -2,5 +2,5 @@ import math
 while True:
     r = float(input('请输入半径: '))
     S= math.pi*math.pow(r,2)
-    print("以%d为半径的圆 面积是%d" % (f,S) )
+    print("以%d为半径的圆 面积是%d" % (r,S) )
     C = 2*math.pi*r
