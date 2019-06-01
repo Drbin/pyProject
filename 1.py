bd8cdf84d8a075da615883823f7f423cc3fec54c
drbin <dairubin0605@163.com>
2019-06-01 15:32:38 +0800

    MQ

--- 1.py
+++ 1.py
@@ -1,7 +1,5 @@
-import math
-while True:
-    r = float(input('请输入半径: '))
-    S = math.pi*math.pow(r, 2)
-    print("以%d为半径的圆 面积是%d" % (r, S))
-    C = 2*math.pi*r
-    print("以%d为半径的圆 周长是%d" % (r, C))
+year = int(input('请输入年份: '))
+# 如果代码太长写成一行不便于阅读 可以使用\或()折行
+is_leap = (year % 4 == 0 and year % 100 != 0 or
+           year % 400 == 0)
+print(is_leap)
