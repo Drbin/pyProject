e472de7184af05ef8071d7e53ec99b7e8f3f6792
drbin <dairubin0605@163.com>
2019-06-02 15:35:59 +0800

    MQ

--- 1.py
+++ 1.py
@@ -1,5 +1,10 @@
-year = int(input('请输入年份: '))
-# 如果代码太长写成一行不便于阅读 可以使用\或()折行
-is_leap = (year % 4 == 0 and year % 100 != 0 or
-           year % 400 == 0)
-print(is_leap)
+while True:
+
+    year = int(input('请输入年份: '))
+    if year =="":
+        print("输入有误")
+        break
+    # 如果代码太长写成一行不便于阅读 可以使用\或()折行
+    is_leap = (year % 4 == 0 and year % 100 != 0 or
+               year % 400 == 0)
+    print(is_leap)
