300a140f6b5d54be2726ba6d6ec0b1ffaa75572b
drbin <dairubin0605@163.com>
2019-06-08 15:50:36 +0800

    MQ

--- 1.py
+++ 1.py
@@ -7,3 +7,4 @@ if a + b > c and a + c > b and b + c > a:
     print('周长: %f' % (a + b + c))
     p = (a + b + c) / 2
     area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+    print('面积: %f' % (area))
