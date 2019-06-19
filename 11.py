24d449d78b18029ae4379d3c2fb534da7c047d26
drbin <dairubin0605@163.com>
2019-06-19 13:58:41 +0800

    MQ

---
+++ 11.py
@@ -0,0 +1,44 @@
+from abc import ABCMeta, abstractmethod
+from random import randint, randrange
+
+
+class Fighter(object, metaclass=ABCMeta):
+    """战斗者"""
+
+    # 通过__slots__魔法限定对象可以绑定的成员变量
+    __slots__ = ('_name', '_hp')
+
+    def __init__(self, name, hp):
+        """初始化方法
+
+        :param name: 名字
+        :param hp: 生命值
+        """
+        self._name = name
+        self._hp = hp
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, hp):
+        self._hp = hp if hp >= 0 else 0
+
+    @property
+    def alive(self):
+        return self._hp > 0
+
+    @abstractmethod
+    def attack(self, other):
+        """攻击
+
+        :param other: 被攻击的对象
+        """
+        pass
+
+
