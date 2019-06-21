67b55b4117d1b3d97d4c1c418e63bdc6d8152e3a
drbin <dairubin0605@163.com>
2019-06-21 14:00:46 +0800

    MQ

--- 11.py
+++ 11.py
@@ -143,3 +143,43 @@ def display_info(ultraman, monsters):
 
 
 
+def main():
+    u = Ultraman('骆昊', 1000, 120)
+    m1 = Monster('狄仁杰', 250)
+    m2 = Monster('白元芳', 500)
+    m3 = Monster('王大锤', 750)
+    ms = [m1, m2, m3]
+    fight_round = 1
+    while u.alive and is_any_alive(ms):
+        print('========第%02d回合========' % fight_round)
+        m = select_alive_one(ms)  # 选中一只小怪兽
+        skill = randint(1, 10)  # 通过随机数选择使用哪种技能
+        if skill <= 6:  # 60%的概率使用普通攻击
+            print('%s使用普通攻击打了%s.' % (u.name, m.name))
+            u.attack(m)
+            print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+        elif skill <= 9:  # 30%的概率使用魔法攻击(可能因魔法值不足而失败)
+            if u.magic_attack(ms):
+                print('%s使用了魔法攻击.' % u.name)
+            else:
+                print('%s使用魔法失败.' % u.name)
+        else:  # 10%的概率使用究极必杀技(如果魔法值不足则使用普通攻击)
+            if u.huge_attack(m):
+                print('%s使用究极必杀技虐了%s.' % (u.name, m.name))
+            else:
+                print('%s使用普通攻击打了%s.' % (u.name, m.name))
+                print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+        if m.alive > 0:  # 如果选中的小怪兽没有死就回击奥特曼
+            print('%s回击了%s.' % (m.name, u.name))
+            m.attack(u)
+        display_info(u, ms)  # 每个回合结束后显示奥特曼和小怪兽的信息
+        fight_round += 1
+    print('\n========战斗结束!========\n')
+    if u.alive > 0:
+        print('%s奥特曼胜利!' % u.name)
+    else:
+        print('小怪兽胜利!')
+
+
+if __name__ == '__main__':
+    main()
