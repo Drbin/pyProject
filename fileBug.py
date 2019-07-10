a89646a27f71843c7d5a7daef57086570d62091d
drbin <dairubin0605@163.com>
2019-07-10 09:46:31 +0800

    MQ

--- fileBug.py
+++ fileBug.py
@@ -8,7 +8,7 @@ def d(url):
     f.write(data)
     f.close()
     file_on("在 %s 获取了数据 并插入 file.html中" % url)
-    print("%d b 数据来源于 %s" %(len(data),url))
+
     return 0
 d(url='https://www.baidu.com')
 
