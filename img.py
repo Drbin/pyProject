4e0c6171a6fada014e68bb83bd7fcf5038935c34
drbin <dairubin0605@163.com>
2019-07-15 10:25:50 +0800

    MQ

--- img.py
+++ img.py
@@ -1,7 +1,8 @@
 from PIL import Image
 from logs import file_on
 
-def img_on():
+def img_on(img):
+    file_on("读取了图片 %s" % img)
     image = Image.open('1.png')
     image.format, image.size, image.mode('JPEG', (500, 750),  'RGB')
     image.show()
