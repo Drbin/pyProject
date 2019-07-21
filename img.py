38ecbe792a68524a248ae11b9d1730f7ea285a79
drbin <dairubin0605@163.com>
2019-07-21 11:04:19 +0800

    MQ

--- img.py
+++ img.py
@@ -1,10 +1,22 @@
-from PIL import Image
-from logs import file_on
+from smtplib import SMTP
+from email.header import Header
+from email.mime.text import MIMEText
 
-def img_on(img):
-    image = Image.open('1.png')
-    img = img.encode()
-    file_on("读取了图片 %s" % img)
-    image.format, image.size, image.mode('JPEG', (500, 750),  'RGB')
-    image.show()
-    file_on("图片成功展示" )
+
+def main():
+    # 请自行修改下面的邮件发送者和接收者
+    sender = 'abcdefg@126.com'
+    receivers = ['uvwxyz@qq.com', 'dairubin0605@163.com']
+    message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
+    message['From'] = Header('王大锤', 'utf-8')
+    message['To'] = Header('骆昊', 'utf-8')
+    message['Subject'] = Header('示例代码实验邮件', 'utf-8')
+    smtper = SMTP('smtp.126.com')
+    # 请自行修改下面的登录口令
+    smtper.login(sender, 'secretpass')
+    smtper.sendmail(sender, receivers, message.as_string())
+    print('邮件发送完成!')
+
+
+if __name__ == '__main__':
+    main()
