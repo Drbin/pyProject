c82e3489f1944be7233b998a59058a4de5a221d5
drbin <dairubin0605@163.com>
2019-08-13 09:54:25 +0800

    MQ

--- fileBug.py
+++ fileBug.py
@@ -1,10 +1,10 @@
 from urllib import request
 from logs import file_on
-def download_file(url):
+def download_file(txt,url):
     print("地址： %s" % url)
     resp = request.urlopen(url)
     data = resp.read()
-    f = open("file.html","wb")
+    f = open(txt ,"wb")
     f.write(data)
     f.close()
     file_on("在 %s 获取了数据 并插入 file.html中 \n" % url)
