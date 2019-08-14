321571c60e545aaace6db8bb7d51ccadb6be6ee4
drbin <dairubin0605@163.com>
2019-08-14 09:53:35 +0800

    MQ

--- fileBug.py
+++ fileBug.py
@@ -1,6 +1,6 @@
 from urllib import request
 from logs import file_on
-def d(url):
+def download_file(url):
     print("地址： %s" % url)
     resp = request.urlopen(url)
     data = resp.read()
@@ -9,7 +9,7 @@ def d(url):
     f.close()
     file_on("在 %s 获取了数据 并插入 file.html中 \n" % url)
     return 0
-d(url='https://www.baidu.com')
+
 
 
 
