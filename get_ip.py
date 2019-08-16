6215890189c149cf487624cbd7509085ac6a6977
drbin <dairubin0605@163.com>
2019-08-16 09:52:00 +0800

    MQ

--- get_ip.py
+++ get_ip.py
@@ -1,4 +1,8 @@
 import socket
+import uuid
+
+mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
+print(":".join([mac[e:e + 2] for e in range(0, 11, 2)]))
 #获取本机电脑名
 myname = socket.getfqdn(socket.gethostname(  ))
 #获取本机ip
