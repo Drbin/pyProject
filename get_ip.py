899ae947b01c741be016ba23107290f20fba239c
drbin <dairubin0605@163.com>
2019-08-17 09:08:08 +0800

    MQ

--- get_ip.py
+++ get_ip.py
@@ -7,6 +7,6 @@ print(":".join([mac[e:e + 2] for e in range(0, 11, 2)]))
 myname = socket.getfqdn(socket.gethostname(  ))
 #获取本机ip
 myaddr = socket.gethostbyname(myname)
-d
+
 print (myname)
 print (myaddr)
