96983771b2cf60d0fbb3ad0f391ea67eefaaab7d
drbin <dairubin0605@163.com>
2019-08-18 12:04:43 +0800

    pymysql 删除

--- pymysql_curd/pmysql_del.py
+++ pymysql_curd/pmysql_del.py
@@ -1,5 +1,6 @@
 import pymysql
 import logs
+
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cur = con.cursor()
 sql_del = "DELETE FROM admin_tbl WHERE admin_id = '22'"
