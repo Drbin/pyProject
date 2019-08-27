e16de2000aabc9890f9ffd12fc0de2eb7112506f
drbin <dairubin0605@163.com>
2019-08-27 09:20:37 +0800

    MQ

--- curd.py
+++ curd.py
@@ -4,8 +4,9 @@ print("pymysql 准备完成")
 con = pymysql.connect(host='localhost', user='root', passwd='root', charset='utf8')
 cursor = con.cursor()
 print("获取光标")
-cursor.execute("use actire_db;")
+con.execute("use actire_db;")
 print("数据库连接")
 sql_insert = 'insert into admin_tbl (admin_name,admin_password) values(11,md5(1));'
-cursor.execute(sql_insert)
+con.execute(sql_insert)
 print("sql语句执行完成")
+
