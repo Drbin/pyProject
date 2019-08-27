947a900e157f987e9748cdde16e1bbd2655b66e8
drbin <dairubin0605@163.com>
2019-08-27 10:35:21 +0800

    MQ

--- curd.py
+++ curd.py
@@ -6,7 +6,7 @@ cursor = con.cursor()
 print("获取光标")
 cursor.execute("use actire_db;")
 print("数据库连接")
-sql_insert = 'insert into admin_tbl(admin_name,admin_password) values(11,md5(1));'
+sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
 cursor.execute(sql_insert)
 print("sql语句执行完成")
 cursor.execute("SELECT VERSION()")
