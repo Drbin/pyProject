5538698e3a1e0ce77ecfd3b137f78dabdcebe0f4
drbin <dairubin0605@163.com>
2019-08-27 10:39:57 +0800

    MQ

--- curd.py
+++ curd.py
@@ -9,11 +9,13 @@ cursor.execute("use actire_db;")
 print("数据库连接")
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
 cursor.execute(sql_insert)
+print(cursor.execute(sql_insert))
 print("sql语句执行完成")
 cursor.execute("SELECT VERSION()")
 
 # 使用 fetchone() 方法获取单条数据.
 data = cursor.fetchone()
+cursor.close()
 
 print("Database version : %s " % data)
 
