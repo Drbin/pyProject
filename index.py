03d39355f1bc57b541f63b9617a481ec8d1d4d54
drbin <dairubin0605@163.com>
2019-08-28 10:02:40 +0800

    pymysql 更新操作

--- index.py
+++
@@ -1,2 +0,0 @@
-from fileBug import download_file
-download_file('in.txt','http://www.baidu.com')
--- single.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-def main():
-    resp = requests.get('http://api.tianapi.com/guonei/?key=APIKey&num=10')
-    data_model = json.loads(resp.text)
-    return data_model
-if __name__ == '__main__':
-    main()
-
