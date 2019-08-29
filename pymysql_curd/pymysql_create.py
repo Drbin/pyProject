9b8f90a5f46793182aefca507480219f8ad3c91f
drbin <dairubin0605@163.com>
2019-08-29 15:04:13 +0800

    pymysql_updata 新增操作

--- pymysql_curd/pymysql_create.py
+++ pymysql_curd/pymysql_create.py
@@ -1,5 +1,5 @@
 import pymysql.cursors
-import logs
+import pymysql_curd.logs as logs
 con = pymysql.connect(host='localhost', user='root', passwd='root', database='actire_db', charset='utf8')
 cursor = con.cursor()
 sql_insert = "insert into admin_tbl(admin_name,admin_login_name,admin_password,admin_create) values (11,1,md5(1),NOW())"
