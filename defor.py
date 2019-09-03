f8e22e69dd261f6ab7eb053a53619e5b39ad7a0d
drbin <dairubin0605@163.com>
2019-09-03 15:54:35 +0800

    pymysql_updata 新增操作

--- defor.py
+++ defor.py
@@ -1,20 +1,14 @@
 import threading
-import time
 
 
 def run(num):
-
     print("running on number:%s" % num)
-
-    time.sleep(3)
 
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=run, args=(1,))
     t2 = threading.Thread(target=run, args=(2,))
-
-    t1.start()  #
-    t2.start()  #
-
-    print(t1.getName())  #
+    t1.start()
+    t2.start()
+    print(t1.getName())  
     print(t2.getName())
