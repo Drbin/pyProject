39930b667a09e3d606e24a87602829184511c20d
drbin <dairubin0605@163.com>
2019-09-07 21:51:43 +0800

    简单的爬虫

--- reptile/int.py
+++ reptile/int.py
@@ -1,5 +1,6 @@
 import urllib.request as urllib2
-req = urllib2.Request("http://www.baidu.com")
+headers = {"User-Agent":""}
+req = urllib2.Request("http://www.baidu.com",headers = headers)
 res = urllib2.urlopen(req)
 html =  res.read()
 print(html)
