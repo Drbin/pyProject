83307a30f58bbdc430ce0c64728c34d235ab84cd
drbin <dairubin0605@163.com>
2019-09-07 21:58:35 +0800

    简单的爬虫

--- reptile/int.py
+++ reptile/int.py
@@ -1,6 +1,11 @@
 import urllib.request as urllib2
-headers = {"User-Agent":""}
+headers = {"User-Agent":""}#user Agent 是反爬虫第一步
+
 req = urllib2.Request("http://www.baidu.com",headers = headers)
 res = urllib2.urlopen(req)
 html =  res.read()
+code = res.getcode()
+print(code)
+#http 响应码
+
 print(html)
