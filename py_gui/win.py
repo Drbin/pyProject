bd582b5768469824c73a2d2588a04563df4a69bb
drbin <dairubin0605@163.com>
2019-09-10 16:35:33 +0800

    pymysql_updata 新增操作

--- py_gui/win.py
+++ py_gui/win.py
@@ -4,7 +4,7 @@ import tkinter as tk  # 使用Tkinter前需要先导入
 window = tk.Tk()
 
 # 第2步，给窗口的可视化起名字
-window.title('My Window')
+window.title('新桌面应用')
 
 # 第3步，设定窗口的大小(长 * 宽)
 window.geometry('500x300')  # 这里的乘是小x
@@ -12,9 +12,16 @@ window.geometry('500x300')  # 这里的乘是小x
 # 第4步，在图形界面上设定标签
 l = tk.Label(window, text='你好！this is Tkinter', bg='green', font=('Arial', 12), width=30, height=2)
 # 说明： bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
+listbox = Listbox(window)
+listbox.pack()
 
+listbox.insert(END, "a list entry")
+
+for item in ["one", "two", "three", "four"]:
+    listbox.insert(END, item)
 # 第5步，放置标签
 l.pack()  # Label内容content区域放置位置，自动调节尺寸
 # 放置lable的方法有：1）l.pack(); 2)l.place();
+window.mainloop()
 
 # 第6步，主窗口循环显示
